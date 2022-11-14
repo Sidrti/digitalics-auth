eb214b2c1f1f9311dd98799b85674753bdc4fe22
Sidrti <41925934+Sidrti@users.noreply.github.com>
2022-11-14 19:03:21 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,39 @@
 <template>
-  <div>
-    <HomeMenu />
-    <div class="main-container">
-      <div class="loginsuccess-container">
-        <h2 class="heading">Login Success!</h2>
-        <div>
-          <div>
-            <div class="center">
-              <p>Welcome {{this.$store.state.loginUser.data.user.name}} !</p>
-              <p><b>Access Token- </b>{{this.$store.state.loginUser.token }}</p>
-            </div>
-          </div>
-        </div>
+  <section class="section-hero section-shaped my-0">
+      <div class="shape shape-style-1 shape-primary">
+          <span class="span-150"></span>
+          <span class="span-50"></span>
+          <span class="span-50"></span>
+          <span class="span-75"></span>
+          <span class="span-100"></span>
+          <span class="span-75"></span>
+          <span class="span-50"></span>
+          <span class="span-100"></span>
+          <span class="span-50"></span>
+          <span class="span-100"></span>
       </div>
-    </div>
-  </div>
+      <div class="container shape-container d-flex align-items-center">
+          <div class="col px-0">
+              <div class="row justify-content-center align-items-center">
+                  <div class="col-lg-7 text-center pt-lg">
+                    <h2 class="text-white font-bold">WELCOME {{this.$store.state.loginUser.data.user.name}} !</h2>
+                      <!-- <img src="img/brand/white.png" style="width: 200px;" class="img-fluid"> -->
+                      <p class="lead text-white mt-4 mb-5">You are all set !</p>
+                      <div class="btn-wrapper">
+                          <base-button tag="a"
+                                       href="https://demos.creative-tim.com/vue-argon-design-system/documentation"
+                                       class="mb-3 mb-sm-0"
+                                       type="info"
+                                       icon="fa fa-code">
+                              Go to extension
+                          </base-button>
+                      </div>
+                  </div>
+              </div>
+             
+          </div>
+      </div>
+  </section>
 </template>
 
 <script>
